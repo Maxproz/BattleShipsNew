6d88730f7fe5a0c8aee08f314aa49eeea52c2e46
Maxpro-PC\Maxpro <maxdietz@hotmail.com>
2018-02-03 08:17:54 -0800

    Done with inital setup basically working....? should I track the component distances in feet? do I decide what units to use? What about between scale and location?

--- Source/BattleShipsNew/BattleShipsNewGameMode.cpp
+++ Source/BattleShipsNew/BattleShipsNewGameMode.cpp
@@ -9,7 +9,7 @@ ABattleShipsNewGameMode::ABattleShipsNewGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/UnTracked/FirstPersonDefaultChar/FirstPerson/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
